dd446a9a845d696ccc37b4752cf7e8066e58966b
Filippo Sallemi <fsallemi@nomadnt.com>
2015-05-25 17:10:17 +0200

    Removed 12.09.x files and directories

--- target/linux/atheros/meos-files/lib/preinit/03_preinit_do_atheros.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-do_atheros() {
-	. /lib/atheros.sh
-
-	atheros_board_detect
-}
-
-boot_hook_add preinit_main do_atheros
--- target/linux/x86/meos-files/lib/preinit/03_preinit_do_x86.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-do_x86() {
-	. /lib/x86.sh
-
-	x86_board_detect
-}
-
-boot_hook_add preinit_main do_x86
